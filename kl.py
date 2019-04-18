fad6938a93c3e6eba301ffb82e860593f141cd4f
i.koryakovskiy <i.koryakovskiy@gmail.com>
2019-04-18 11:16:37 +0300

    Adding KL divergence

---
+++ kl.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 11 11:48:40 2019
+
+@author: Koryakovskiy Ivan (i.koryakovskiy@gmail.com)
+"""
+
+import numpy as np
+import scipy.stats
+import matplotlib.pyplot as plt
+fig, axis = plt.subplots(1, 1)
+
+n = 100
+eps = 1e-20
+
+da = scipy.stats.norm(0, 1)
+db = scipy.stats.norm(1, 1)
+
+xa = da.rvs(n)
+pa = da.pdf(xa) + eps
+pb = db.pdf(xa) + eps
+
+kl = np.mean(pa*np.log(pa/pb))
+print("KL: " + str(kl))
+
+axis.hist(xa, density=True, histtype='stepfilled', alpha=0.5)
+plt.show()
